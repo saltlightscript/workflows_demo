e929702e9890da41426490a43f4e9ad5b09766c8
saltlightscript <86891834+saltlightscript@users.noreply.github.com>
2022-01-17 03:35:39 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello Wold!")
+import sys
 
+def main():
+    print("Hello Wold! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3,6) and sys.version_info < (3,7):
+        # Lets make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is not supported!')
+        
 if __name__ == "__main__":
     main()
